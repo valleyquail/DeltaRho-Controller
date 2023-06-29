adb57be79a80e02fa0d7fcc9cd47d6d6d346c88f
Nikesh Walling <ngwalling@wpi.edu>
2023-06-29 19:07:27 -0400

    Removing wireless from this branch

--- cmake-build-debug/CMakeFiles/3.25.2/CMakeSystem.cmake
+++ cmake-build-debug/CMakeFiles/3.25.2/CMakeSystem.cmake
@@ -1,6 +1,6 @@
-set(CMAKE_HOST_SYSTEM "Linux-5.19.0-42-generic")
+set(CMAKE_HOST_SYSTEM "Linux-5.19.0-46-generic")
 set(CMAKE_HOST_SYSTEM_NAME "Linux")
-set(CMAKE_HOST_SYSTEM_VERSION "5.19.0-42-generic")
+set(CMAKE_HOST_SYSTEM_VERSION "5.19.0-46-generic")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
 include("/home/nikesh/Documents/pico/pico-sdk/cmake/preload/toolchains/pico_arm_gcc.cmake")
--- lib/MQTT_Client.h
+++ lib/MQTT_Client.h
@@ -5,7 +5,7 @@
 #ifndef DELTARHO_CONTROLLER_MQTT_CLIENT_H
 #define DELTARHO_CONTROLLER_MQTT_CLIENT_H
 
-#include "lwip/apps/mqtt.h"
+// #include "lwip/apps/mqtt.h"
 #include <stdlib.h>
 #include <string>
 
